0c47e51fbdf48546c1593039e7a922f6fbbba853
Jason Ji <uberjason@gmail.com>
2015-06-25 12:32:50 -0400

    Initial Commit

---
+++ SelfSizingNightmare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SelfSizingNightmare
+//
+//  Created by Jason Ji on 6/25/15.
+//  Copyright (c) 2015 Jason Ji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
